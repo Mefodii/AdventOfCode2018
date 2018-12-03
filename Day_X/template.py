b61f3a806be6893b44cce1a748e971fe3809cb81
Stavila Alexandru <alexandru.stavila@fortech.ro>
2018-12-03 18:11:42 +0200

    Sample file possibility

--- Day_X/template.py
+++ Day_X/template.py
@@ -13,8 +13,10 @@ from utils.helpers import Runner
 PARENT_FOLDER_NAME = "Day_1"
 FILES_NAME = "io"
 INPUT_FILE = "input.txt"
+SAMPLE_FILE = "sample.txt"
 OUTPUT_FILE = os.path.basename(__file__)[:-3] + "_output.txt"
 
+SAMPLE_PATH = "/".join([paths.PROJECT_PATH, PARENT_FOLDER_NAME, FILES_NAME, SAMPLE_FILE])
 INPUT_PATH = "/".join([paths.PROJECT_PATH, PARENT_FOLDER_NAME, FILES_NAME, INPUT_FILE])
 OUTPUT_PATH = "/".join([paths.PROJECT_PATH, PARENT_FOLDER_NAME, FILES_NAME, OUTPUT_FILE])
 
